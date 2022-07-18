9d6a8a3df434df98bbd93b83f8f448cc19223e74
Furkan Mudanyali <fmudanyali@icloud.com>
2022-07-18 09:59:04 +0300

    Initial commit

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/rockos.kernel isodir/boot/rockos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "rockOS" {
+    multiboot /boot/rockos.kernel
+}
+EOF
+grub2-mkrescue -o rockos.iso isodir
